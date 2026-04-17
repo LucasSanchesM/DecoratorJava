c75b549b5bbc4416aace77ff5e25ee4f80a7f028
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-04-17 14:04:01 -0300

    Override exibirDescricao method in AvaliacaoOtima

--- exemplo02/AvaliacaoOtima.java
+++ exemplo02/AvaliacaoOtima.java
@@ -4,9 +4,8 @@ public class AvaliacaoOtima extends AdicionarAvaliacao{
     public AvaliacaoOtima(Local q){
         super(q);
     }
-    
+    @Override
     public String exibirDescricao(){
         return local.exibirDescricao()+ " e tem uma otima avaliacao";
-    }
-    
+    }   
 }
