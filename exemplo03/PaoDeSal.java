ca845f8e2d0b687704fe538d9c1354fe778ab988
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-04-17 17:28:39 -0300

    Add PaoDeSal class implementing Comida interface

---
+++ exemplo03/PaoDeSal.java
@@ -0,0 +1,15 @@
+package padaria.sonho;
+
+public class PaoDeSal implements Comida{
+    double preco=1.0;
+    @Override
+    public String descricao(){
+        return "Pao de sal";
+    }
+    
+    @Override
+    public double preco(){
+        return preco;
+    }
+    
+}
